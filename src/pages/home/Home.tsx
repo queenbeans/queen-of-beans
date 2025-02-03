bb4103745c8ec1c24cac6ec4501e92daacdce38a
Queen beans <14365623+queenbeans@users.noreply.github.com>
2025-02-03 15:59:57 -0500

    update home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useParams } from "react-router";
 import { Layout, NavBar } from "../../components";
 import { pageRoutes } from "../../useRouter";
-import { DisclaimerText } from "../../components/content";
+import { DisclaimerText, Page, Paragraph } from "../../components/content";
 
 export const Home = () => {
   const params = useParams();
@@ -13,7 +13,18 @@ export const Home = () => {
           path: route.path,
         }))}
       />
-      {params["*"] && <Outlet />}
+      {params["*"] ? (
+        <Outlet />
+      ) : (
+        <Page>
+          <Paragraph>
+            Hello! My name is Davian (aka Queen Beans). I created this website
+            to showcase my personal programming and art practices. This site
+            also functions as a way to experiment with different web
+            technologies. Feel free to explore the site and check out my work!
+          </Paragraph>
+        </Page>
+      )}
       <DisclaimerText>
         Note, this website makes use of client side routing via React Router and
         is served using Github pages. If you attempt to refresh the page on a
