bbe39958f00a9ab446dd751545d5759ab76504c0
Queen beans <14365623+queenbeans@users.noreply.github.com>
2025-02-03 15:02:35 -0500

    move site disclaimer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useParams } from "react-router";
 import { Layout, NavBar } from "../../components";
 import { pageRoutes } from "../../useRouter";
+import { Paragraph } from "../../components/content";
 
 export const Home = () => {
   const params = useParams();
@@ -13,6 +14,12 @@ export const Home = () => {
         }))}
       />
       {params["*"] && <Outlet />}
+      <Paragraph>
+        Note, this website makes use of client side routing via React Router and
+        is served using Github pages. If you attempt to refresh the page on a
+        route other than the root, you will receive a 404 error. This is
+        expected behavior as Github pages does not support server side routing.
+      </Paragraph>
     </Layout>
   );
 };
